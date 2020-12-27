6b34be1a7ac330960e1793649b908bf7beba3376
Aya Abdelrahman <aya.a.mahmoud79@gmail.com>
2020-12-27 17:34:08 +0200

    function CheckRows was added

--- tictactoeFuncs.py
+++ tictactoeFuncs.py
@@ -1,7 +1,6 @@
 
 
-# user picks either X or O 
-# I/O 
+
 def pickLetter():
     letter = input("Choose X or O: ")
     while True:    
@@ -16,8 +15,7 @@ def pickLetter():
 
 
 
-# checks the diagonals for a win
-# list -> bool, string
+
 def checkDiags(board):
     if board[0] == board[4] == board[8] and board[0] != " ":
         return True, board[0]
@@ -25,5 +23,18 @@ def checkDiags(board):
         return True, board[2]
     else: 
         return False, board[0]
+
+def checkRows(board):
+    if board[0] == board[1] == board[2] and board[2] !=" ":
+       return True,board[0]
+    elif board[3]==board[4] == board[5] and board[5] !=" " :
+        return True,board[3]
+    elif board[6]==board[7]==board[8] and board[8] != " " :
+        return True,board[6]
+    else:
+        return False,board[0]
+
+
+
   
 
